949246d76b9702b2f9153db9252003f66e6ab673
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-01-09 08:40:52 -0800

    adding and removing to/from cache api

--- cache/main.js
+++ cache/main.js
@@ -1,15 +1,24 @@
+// if (window.caches) {
+//   /* INITIALIZE CAHCHES */
+//   caches.open("test1");
+//   caches.open("test2");
+
+//   /* GET ALL OPEN CACHES */
+//   caches.keys().then(console.log);
+
+//   /* CHECK IF CACHE EXISTS */
+//   caches.has("test1").then(console.log);
+//   caches.has("test3").then(console.log);
+
+//   /* DELETE CACHE */
+//   caches.delete("test1").then(console.log);
+// }
+
 if (window.caches) {
-  /* INITIALIZE CAHCHES */
-  caches.open("test1");
-  caches.open("test2");
-
-  /* GET ALL OPEN CACHES */
-  caches.keys().then(console.log);
-
-  /* CHECK IF CACHE EXISTS */
-  caches.has("test1").then(console.log);
-  caches.has("test3").then(console.log);
-
-  /* DELETE CACHE */
-  caches.delete("test1").then(console.log);
+  caches.open("pwa-v1.1").then(cache => {
+    /* ADDING FILES/ REQUESTS TO THE CACHE */
+    //cache.addAll(["./index.html", "./style.css", "main.js"]);
+    /* REMOVING FROM THE CACHE */
+    //cache.delete("./style.css");
+  });
 }
